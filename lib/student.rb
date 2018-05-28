3bc13a2353dcc4d8f4ce0173453a592b8d6d81e7
Ji Yim <gsooyim@gmail.com>
2018-05-28 22:42:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new = Student.new
+    new_student = self.new
+    new_student.name = 
   end
 
   def self.all
