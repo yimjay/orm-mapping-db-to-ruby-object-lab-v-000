4eae94a44fdd6d7d23c1bbeb2e8088df25f0cac3
Ji Yim <gsooyim@gmail.com>
2018-05-28 22:57:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
     end
   end
   
-  def self.first_x_students_in_grade_10(num)
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
       SELECT * FROM students WHERE grade=10 LIMIT ?
     SQL
@@ -68,7 +68,7 @@ class Student
     end
   end
   
-  def self.all_students_in_grade_x(grade)
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * FROM students WHERE grade=?
     SQL
